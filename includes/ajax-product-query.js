8442b4781ba4547156b782be28292715d5dcd1c0
HopeSaurus <hopeXime@outlook.com>
2023-07-27 12:30:17 -0600

    Lets see if this works

--- includes/ajax-product-query.js
+++ includes/ajax-product-query.js
@@ -37,8 +37,8 @@ jQuery(document).ready(function($) {
     
                             products.forEach(function(product){
     
-                                let latitude = parseFloat(product.latitude) + ((Math.random() - 0.5) * 0.0002);
-                                let longitude = parseFloat(product.longitude) + ((Math.random() - 0.5) * 0.0002);
+                                let latitude = parseFloat(product.latitude) + ((Math.random() - 0.5) * 0.02);
+                                let longitude = parseFloat(product.longitude) + ((Math.random() - 0.5) * 0.02);
     
                                 if(latitude && longitude /*&& (markersIDs[product.ID]==undefined)*/){
     
