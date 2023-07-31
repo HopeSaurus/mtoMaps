78c12f3bda68752da1c48ef0556175202ac324fb
HopeSaurus <hopeXime@outlook.com>
2023-07-31 16:34:46 -0600

    Fix query selector typo

--- includes/mtoMaps.js
+++ includes/mtoMaps.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
   const mapCenterButton = document.getElementById('map-center-button');
   const filterButton = document.querySelector('categories-list__title');
-  const filterMenu = document.querySelectorAll('subcategories-wrapper');
+  const filterMenu = document.querySelectorAll('categories-wrapper');
 
   function centerMap(){
     if(clusterBounds){
