cbb7309422ed508e5d99c86c1f9a546ee3e5cf60
HopeSaurus <hopeXime@outlook.com>
2023-07-31 12:36:57 -0600

    Fetch categories starting with parent category

--- products-categories-query.php
+++ products-categories-query.php
@@ -1,31 +1,51 @@
 <?php
 // Function to display product category checkboxes
 function display_product_category_checkboxes() {
-    
+
     $filter_img_url = plugins_url('/',__FILE__) . '/assets/filtro.png';
 
-    $args = array(
-        'taxonomy' => 'product_cat',
-        'hide_empty' => true,
-    );
+    $parent_categories = get_parent_categories();
 
-    $categories = get_terms($args);
 
-    if (!empty($categories)) {
+    if(!empty($parent_categories)){
         echo '<div class="categories-container">';
-        echo '<div class="categories-list__title"><img src=' . $filter_img_url . '></img>Filtrar</div>';
-        echo '<ul class="categories-list">';
-        foreach ($categories as $category) {
-            echo '<li><label><input type="checkbox" name="product_category"  data-category-slug="' . $category->slug . '"> ' . $category->name . '</label></li>';
+        foreach($parent_categories as $parent_category){
+            echo'<div class="categories-container--parent-category">'. $parent_category .'</div>'; 
+            $args = array(
+                'taxonomy' => 'product_cat',
+                'child_of' => $parent_category->term_id,
+                'hide_empty' => 1, 
+            );
+
+            $subcategories = get_terms($args);
+
+            if(!empty($subcategories)){
+                echo '<ul class="subcategories-list">';
+                foreach($subcategories as $subcategory){
+                    echo '<li><label><input type="checkbox" name="product_category"  data-category-slug="' . $subcategory->slug . '"> ' . $subcategory->name . '</label></li>';
+                }
+                echo '<li><label><div class="clean-checkboxes" >Limpiar</div></label></li>';
+                echo '</ul>';
+            }
         }
-        echo '<li><label><div class="clean-checkboxes" >Limpiar</div></label></li>';
-        echo '</ul>';
         echo '</div>';
-    } else {
-        echo 'No categories found.';
     }
+
 }
 
 add_shortcode('product_categories_listing', 'display_product_category_checkboxes');
+
+function get_parent_categories(){
+    $args = array(
+        'taxonomy' => 'product_cat',
+        'parent' => 0, // Set to 0 to get only top-level parent categories
+        'hide_empty' => 1, // Set to 1 to exclude parent categories with no products assigned
+    );
+
+    $parent_categories = get_terms($args);
+
+    return $parent_categories;
+    
+}
 
 ?>
