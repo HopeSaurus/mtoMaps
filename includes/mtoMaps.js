a44a8f25a06cdf714db35fd5a19b3b000db036e4
HopeSaurus <hopeXime@outlook.com>
2023-07-31 17:29:51 -0600

    Add out of focus menu events

--- includes/mtoMaps.js
+++ includes/mtoMaps.js
@@ -10,9 +10,19 @@ jQuery(document).ready(function($) {
   }
 
   function activateFilterMenu(){
-    filterMenu.classList.add('activate-menu');
+    if(filterMenu.classList.contains('activate-menu')){
+      filterMenu.classList.remove('activate-menu');
+    }else filterMenu.classList.add('activate-menu');
   }
 
   mapCenterButton.addEventListener('click', centerMap);
   filterButton.addEventListener('click', activateFilterMenu);
+
+  document.addEventListener('click', function(event) {
+    // Check if the click target is not a descendant of the menu element
+    if (!filtermenu.contains(event.target) && filterMenu.classList.contains('activate-menu')) {
+      // Click occurred outside of the menu, do something here (e.g., close the menu, hide a dropdown, etc.)
+      filterMenu.classList.remove('activate-menu');
+    }
+  });
 });
